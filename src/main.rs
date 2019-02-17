8390bb8ec1522ea7de6d45eb0a0f1384e5def258
Jonathan Stites <mail@jonstites.com>
2019-02-17 14:39:25 -0600

    Adds a silly test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn silly_function() -> i32 {
+    42
+}
+
 fn main() {
     println!("Hello, ci/cd!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_silly_function() {
+        assert_eq!(silly_function(), 42);
+    }
 }
